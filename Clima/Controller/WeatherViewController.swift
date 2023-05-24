0064272ad0490c99d47ae3d3c2728280958b5e84
noramari <noora@selinan.com>
2023-05-24 17:41:05 +0900

    add weather manager and json handler

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -15,6 +15,8 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var searchTextField: UITextField!
     
+    var weatherManager = WeatherManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -40,7 +42,9 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
         searchTextField.text = ""
     }
     
---
+++ Clima/Model2/WeatherManager2.swift
@@ -0,0 +1,44 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Noora Maeda on 2023/05/24.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?&appid=27915efc66675dae54e2a5008f8cc819&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            
+        } catch {
+            print(error)
+        }
+    }
+}
